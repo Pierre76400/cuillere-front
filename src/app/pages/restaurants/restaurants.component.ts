0799eeaab839a67d2859189ca5157c63d4bd9697
OUERGHI Bilel <bilel.ouerghi@softeam.fr>
2024-02-23 11:02:47 +0100

    add pagination and new component FAQ

--- src/app/pages/restaurants/restaurants.component.ts
+++ src/app/pages/restaurants/restaurants.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import {catchError, throwError} from "rxjs";
+import { catchError, throwError } from "rxjs";
+import { RechercheRestaurantDto } from 'src/app/models/RechercheRestaurantDto';
 import { RestaurantDto } from 'src/app/models/RestaurantDto';
 import { RestaurantService } from 'src/app/services/restaurant.service';
 
@@ -10,50 +11,84 @@ import { RestaurantService } from 'src/app/services/restaurant.service';
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.scss'],
 })
-export class RestaurantsComponent implements OnInit  {
+export class RestaurantsComponent implements OnInit {
 
-  numPage! : number;
-  taillePage!: number;
-  nomRestaurant:String =""
 
-  searchFormGroup : FormGroup | undefined;
+  pageSize!: number;
+  nomRestaurant: String = "";
+  nbResultat!: any;
+  currentPage: number = 1;
+  pages: number[] = [];
+
+  searchFormGroup: FormGroup | undefined;
   restaurants: RestaurantDto[] | undefined;
-  
-  constructor(private restaurantService : RestaurantService, private fb : FormBuilder,
+  rechercheRestaurant: RechercheRestaurantDto | undefined;
+
+  constructor(private restaurantService: RestaurantService, private fb: FormBuilder,
     private router: Router) { }
   ngOnInit(): void {
 
-    this.searchFormGroup=this.fb.group({
-      nomRestaurant : this.fb.control("")
+    this.searchFormGroup = this.fb.group({
+      nomRestaurant: this.fb.control("")
     });
     this.getRestaurants();
     this.handleSearchRestaurants();
   }
   handleSearchRestaurants() {
-    let nomRestaurant=this.searchFormGroup?.value
-    .nomRestaurant;
-  this.restaurantService.searchRestaurants(nomRestaurant).subscribe(data=>{
- this.restaurants =data.restaurants
-  })
-      catchError(err => {
+    let nomRestaurant = this.searchFormGroup?.value
+      .nomRestaurant;
+    if (this.isPaginatedSearch()) {
+      // Recherche paginée
+      this.restaurantService.searchRestaurantsWithPagination(nomRestaurant, this.currentPage, this.pageSize).subscribe(data => {
+        this.restaurants = data.restaurants;
+        this.nbResultat = data.nbResultat;
+      }, err => {
         return throwError(err);
-      })
+      });
+    } else {
+      // Recherche non paginée
+      this.restaurantService.searchRestaurants(nomRestaurant).subscribe(data => {
+        this.restaurants = data.restaurants;
+      }, err => {
+        return throwError(err);
+      });
+    }
 
+  }
+
+  gotoPage(page: number) {
+    this.currentPage = page;
+    this.handleSearchRestaurants();
   }
 
   getRestaurants() {
-    this.restaurantService.getRestaurants().subscribe(data=>{
-      this.restaurants =data;
+    this.restaurantService.getRestaurants().subscribe(data => {
+      this.restaurants = data;
 
     })
-      catchError(err => {
-        return throwError(err);
-      })
+    catchError(err => {
+      return throwError(err);
+    })
   }
 
-  onKeyUpEvent(event: any){
+  onKeyUpEvent(event: any) {
     this.handleSearchRestaurants();
- }
+  }
+
+  // Méthode pour calculer le nombre total de pages
+  getTotalPages(): number {
+    if (this.nbResultat && this.pageSize) {
+      return Math.ceil(this.nbResultat / this.pageSize);
+    } else {
+      return 0;
+    }
+  }
+
+  isPaginatedSearch(): boolean {
+    // Vérifie si la pagination est activée (dans cet exemple, si pageSize est défini)
+    return !!this.pageSize;
+  }
+
 }
 
 
