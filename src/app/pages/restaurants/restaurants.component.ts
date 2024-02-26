f71ca7546233444a3a71478054e7f0383edc42e7
Bragon76 <pierre.leplay@gmail.com>
2024-02-26 15:13:05 +0100

    Split en 2 écrans de recherches etape2

--- src/app/pages/restaurants/restaurants.component.ts
+++ src/app/pages/restaurants/restaurants.component.ts
@@ -13,67 +13,41 @@ import { RestaurantService } from 'src/app/services/restaurant.service';
 })
 export class RestaurantsComponent implements OnInit {
 
-  pageSize: number = 10;
   nomRestaurant: String = "";
-  nbResultat!: any;
-  currentPage!: number;
-  pages: number[] = [];
-  paginationRange: number = 5;
-
-
   searchFormGroup: FormGroup | undefined;
   restaurants: RestaurantDto[] | undefined;
-  rechercheRestaurant: RechercheRestaurantDto | undefined;
 
   constructor(private restaurantService: RestaurantService, private fb: FormBuilder,
     private router: Router) { }
+
   ngOnInit(): void {
-    this.currentPage = 0;
     this.searchFormGroup = this.fb.group({
       nomRestaurant: this.fb.control("")
     });
     this.getRestaurants();
     this.handleSearchRestaurants();
   }
+
   handleSearchRestaurants() {
-    let nomRestaurant = this.searchFormGroup?.value
-      .nomRestaurant;
-    if (this.isPaginatedSearch()) {
-      // Recherche paginée
-      this.restaurantService.searchRestaurantsWithPagination(nomRestaurant, this.currentPage, this.pageSize).subscribe(data => {
-        this.restaurants = data.restaurants;
-        this.nbResultat = data.nbResultat;
-        this.completerNbAvis();
-      }, err => {
-        return throwError(err);
-      });
-    } else {
-      // Recherche non paginée
-      this.restaurantService.searchRestaurants(nomRestaurant).subscribe(data => {
-        this.restaurants = data.restaurants;
-      }, err => {
-        return throwError(err);
-      });
-    }
+    let nomRestaurant = this.searchFormGroup?.value.nomRestaurant;
+    this.restaurantService.searchRestaurants(nomRestaurant).subscribe(data => {
+      this.restaurants = data.restaurants;
+    }, err => {
+      return throwError(err);
+    });
+
   }
 
-  completerNbAvis(){
-    if(this.restaurants!=undefined){
-      for(let i=0;i<this.restaurants.length;i++){
-        let r=this.restaurants[i];
+  completerNbAvis() {
+    if (this.restaurants != undefined) {
+      for (let i = 0; i < this.restaurants.length; i++) {
+        let r = this.restaurants[i];
         this.restaurantService.getAvis(r.id).subscribe(
-          data=> {
-            r.nbAvis=data.length;
+          data => {
+            r.nbAvis = data.length;
           }
         );
       }
-    }
-  }
-
-  gotoPage(page: number) {
-    if (page >= 0 && page < this.getTotalPages()) {
-      this.currentPage = page;
-      this.handleSearchRestaurants();
     }
   }
 
@@ -89,37 +63,6 @@ export class RestaurantsComponent implements OnInit {
 
   onKeyUpEvent(event: any) {
     this.handleSearchRestaurants();
-  }
-
-  // Méthode pour calculer le nombre total de pages
-  getTotalPages(): number {
-    if (this.nbResultat && this.pageSize) {
-      return Math.ceil(this.nbResultat / this.pageSize);
-    } else {
-      return 0;
-    }
-  }
-
-  isPaginatedSearch(): boolean {
-    // Vérifie si la pagination est activée (dans cet exemple, si pageSize est défini)
-    return !!this.pageSize;
-  }
-  getPaginationStartIndex(): number {
-    let start = this.currentPage - Math.floor(this.paginationRange / 2);
-    return Math.max(start, 0);
-  }
-
-  // Méthode pour obtenir l'indice de fin de la plage de pagination
-  getPaginationEndIndex(): number {
-    let end = this.currentPage + Math.ceil(this.paginationRange / 2);
-    return Math.min(end, this.getTotalPages() - 1);
-  }
-
-  // Méthode pour obtenir la liste des pages à afficher
-  getDisplayedPages(): number[] {
-    const startIndex = this.getPaginationStartIndex();
-    const endIndex = this.getPaginationEndIndex();
-    return Array.from({ length: endIndex - startIndex + 1 }, (_, i) => startIndex + i);
   }
 
 }
