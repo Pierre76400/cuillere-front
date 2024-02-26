4716d78efcd2937cf6e4aca83c6be9e4dfd00c35
Bragon76 <pierre.leplay@gmail.com>
2024-02-26 15:34:31 +0100

    Split en 2 écrans de recherches etape3

--- src/app/pages/restaurantsPagines/restaurantsPagines.component.ts
+++ src/app/pages/restaurantsPagines/restaurantsPagines.component.ts
@@ -13,57 +13,46 @@ import { RestaurantService } from 'src/app/services/restaurant.service';
 })
 export class RestaurantsPaginesComponent implements OnInit {
 
-pageSize: number = 10;
+  pageSize: number = 10;
   nomRestaurant: String = "";
   nbResultat!: any;
   currentPage!: number;
-  pages: number[] = [];  
   paginationRange: number = 5;
 
 
   searchFormGroup: FormGroup | undefined;
   restaurants: RestaurantDto[] | undefined;
-  rechercheRestaurant: RechercheRestaurantDto | undefined;
 
   constructor(private restaurantService: RestaurantService, private fb: FormBuilder,
     private router: Router) { }
   ngOnInit(): void {
-this.currentPage = 0;
+    this.currentPage = 0;
     this.searchFormGroup = this.fb.group({
       nomRestaurant: this.fb.control("")
     });
-    this.getRestaurants();
-    this.handleSearchRestaurants();
+    //this.handleSearchRestaurants();
   }
+
   handleSearchRestaurants() {
     let nomRestaurant = this.searchFormGroup?.value
       .nomRestaurant;
-    if (this.isPaginatedSearch()) {
-      // Recherche paginée
-      this.restaurantService.searchRestaurantsWithPagination(nomRestaurant, this.currentPage, this.pageSize).subscribe(data => {
-        this.restaurants = data.restaurants;
-        this.nbResultat = data.nbResultat;
-        this.completerNbAvis();
-      }, err => {
-        return throwError(err);
-      });
-    } else {
-      // Recherche non paginée
-      this.restaurantService.searchRestaurants(nomRestaurant).subscribe(data => {
-        this.restaurants = data.restaurants;
-      }, err => {
-        return throwError(err);
-      });
-    }
+    // Recherche paginée
+    this.restaurantService.searchRestaurantsWithPagination(nomRestaurant, this.currentPage, this.pageSize).subscribe(data => {
+      this.restaurants = data.restaurants;
+      this.nbResultat = data.nbResultat;
+      this.completerNbAvis();
+    }, err => {
+      return throwError(err);
+    });
   }
 
-  completerNbAvis(){
-    if(this.restaurants!=undefined){
-      for(let i=0;i<this.restaurants.length;i++){
-        let r=this.restaurants[i];
+  completerNbAvis() {
+    if (this.restaurants != undefined) {
+      for (let i = 0; i < this.restaurants.length; i++) {
+        let r = this.restaurants[i];
         this.restaurantService.getAvis(r.id).subscribe(
-          data=> {
-            r.nbAvis=data.length;
+          data => {
+            r.nbAvis = data.length;
           }
         );
       }
@@ -71,24 +60,10 @@ this.currentPage = 0;
   }
 
   gotoPage(page: number) {
-if (page >= 0 && page < this.getTotalPages()) {
-    this.currentPage = page;
-    this.handleSearchRestaurants();
-  }
-}
-
-  getRestaurants() {
-    this.restaurantService.getRestaurants().subscribe(data => {
-      this.restaurants = data;
-
-    })
-    catchError(err => {
-      return throwError(err);
-    })
-  }
-
-  onKeyUpEvent(event: any) {
-    this.handleSearchRestaurants();
+    if (page >= 0 && page < this.getTotalPages()) {
+      this.currentPage = page;
+      this.handleSearchRestaurants();
+    }
   }
 
   // Méthode pour calculer le nombre total de pages
